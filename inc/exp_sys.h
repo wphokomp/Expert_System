9b41175f126d14062e61070f6f699659b438fb6e
lmucassi <lindasmucassi@gmai.com>
2018-01-16 09:17:22 +0200

    "added_a_gitignr_file"

--- inc/exp_sys.h
+++ inc/exp_sys.h
@@ -16,12 +16,7 @@
 # include "../libft/libft.h"
 # include <stdio.h>
 # include <stdlib.h>
-<<<<<<< HEAD
-=======
-# define FILE_ERR = 1
-# define FORMT_ERR = 2
-/* all structs here */
->>>>>>> 17e663ecad853c0c3218863c2e8d4cdc05f453da
+# include <stdbool.h>
 
 typedef	struct		s_shunt
 {
@@ -42,14 +37,11 @@ int					find_query();
 
 char				**express(t_shunt *shnt);
 
-<<<<<<< HEAD
 void				tokens(t_shunt *shnt);
 void				get_data(int fd, char *file, t_shunt *shnt);
-=======
 /* all void funcs */
 void	get_err(int err_num);
 void	tokens(t_shunt *shnt);
 void	get_data(int fd, char *file, t_shunt *shnt);
->>>>>>> 17e663ecad853c0c3218863c2e8d4cdc05f453da
 
 #endif
