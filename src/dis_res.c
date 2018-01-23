48f58805d7a2370773d9b469a54ce0df5c5b8e2a
lmucassi <lindasmucassi@gmai.com>
2018-01-23 16:20:57 +0200

    "coloring"

--- src/dis_res.c
+++ src/dis_res.c
@@ -6,7 +6,7 @@
 /*   By: lmucassi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/23 16:00:51 by lmucassi          #+#    #+#             */
-/*   Updated: 2018/01/23 16:18:46 by lmucassi         ###   ########.fr       */
+/*   Updated: 2018/01/23 16:20:33 by lmucassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,7 @@ void	final(t_shunt *shnt)
 {
 	ft_putendl("\x1b[33m=============================================\x1b[0m");
 	ft_putendl("\x1b[32m\tCalculating Results From Expressions\x1b[0m");
-	/*
- 	* 	call process() to dispaly all the results here */
- 	 process(shnt);
- 	
+ 	process(shnt);	
 	ft_putendl("\x1b[32m\t\tDone!!!\x1b[0m");
 	ft_putendl("\x1b[33m=============================================\x1b[0m");
 }
