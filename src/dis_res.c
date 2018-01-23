e4496f59e5ee71c2ecfcf6ffe3891d258826aec7
lmucassi <lindasmucassi@gmai.com>
2018-01-23 16:19:45 +0200

    "indenting"

--- src/dis_res.c
+++ src/dis_res.c
@@ -6,7 +6,7 @@
 /*   By: lmucassi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/23 16:00:51 by lmucassi          #+#    #+#             */
-/*   Updated: 2018/01/23 16:09:36 by lmucassi         ###   ########.fr       */
+/*   Updated: 2018/01/23 16:18:46 by lmucassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	final(t_shunt *shnt)
 {
-	ft_putendl("\x1b[33m=========================================\x1b[0m");
-	ft_putendl("\x1b[33m\tCalculating Results From Expressions\x1b[0m");
+	ft_putendl("\x1b[33m=============================================\x1b[0m");
+	ft_putendl("\x1b[32m\tCalculating Results From Expressions\x1b[0m");
 	/*
  	* 	call process() to dispaly all the results here */
  	 process(shnt);
  	
-	ft_putendl("\x1b[33m\t\t\tDone!!!\x1b[0m");
-	ft_putendl("\x1b[33m=========================================\x1b[0m");
+	ft_putendl("\x1b[32m\t\tDone!!!\x1b[0m");
+	ft_putendl("\x1b[33m=============================================\x1b[0m");
 }
